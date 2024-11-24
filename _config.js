b6c7d16b547bd24dbf263dc37bda9c2782d4da16
Mugao-joy <mugaojoy@gmail.com>
2024-11-24 10:46:15 +0300

    milestone 2

--- _config.js
+++ _config.js
@@ -2,8 +2,8 @@ var config = {}
 
 // Update to have your correct username and password
 config.mongoURI = {
-    production: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom?retryWrites=true&w=majority',
-    development: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
-    test: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom-test?retryWrites=true&w=majority',
+    production: 'mongodb+srv://mugaodev:Dp1PsAf3bmbY2EZU@devops08joy.qbedo.mongodb.net/',
+    development: 'mongodb+srv://mugaodev:Dp1PsAf3bmbY2EZU@devops08joy.qbedo.mongodb.net/',
+    test: 'mongodb+srv://mugaodev:Dp1PsAf3bmbY2EZU@devops08joy.qbedo.mongodb.net/',
 }
 module.exports = config;
--- server.js
+++ server.js
@@ -3,44 +3,38 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const path = require('path');
 
-// Define routes
-let index = require('./routes/index');
-let image = require('./routes/image');
+// Import configuration
+const config = require('./_config');
 
-// connecting the database
-let mongodb_url = 'mongodb://localhost:27017/';
-let dbName = 'darkroom';
-mongoose.connect(`${mongodb_url}${dbName}`,{ useNewUrlParser: true , useUnifiedTopology: true }, (err)=>{
-    if (err) console.log(err)
-});
+// Determine environment
+const env = process.env.NODE_ENV || 'development'; // Defaults to 'development'
+const mongodb_url = config.mongoURI[env]; // Get the MongoDB URI from config.js
 
-// test if the database has connected successfully
-let db = mongoose.connection;
-db.once('open', ()=>{
-    console.log('Database connected successfully')
-})
+// Connecting to the database
+mongoose.connect(mongodb_url, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log(`Database connected successfully to ${env} environment`))
+  .catch((err) => console.error('Database connection error:', err));
 
-// Initializing the app
+// Initialize the app
 const app = express();
-
 
 // View Engine
 app.set('view engine', 'ejs');
 
-// Set up the public folder;
+// Set up the public folder
 app.use(express.static(path.join(__dirname, 'public')));
 
-// body parser middleware
-app.use(express.json())
+// Body parser middleware
+app.use(express.json());
 
+// Define routes
+const index = require('./routes/index');
+const image = require('./routes/image');
 
 app.use('/', index);
 app.use('/image', image);
 
-
-
- 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT,() =>{
-    console.log(`Server is listening at http://localhost:${PORT}`)
+app.listen(PORT, () => {
+    console.log(`Server is listening at http://localhost:${PORT}`);
 });
